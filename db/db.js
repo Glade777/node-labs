9d31d5de6af9f9938f7320f8729f93afd89a4e69
Glade777 <kyrychenkodaoff@gmail.com>
2026-03-12 20:38:08 +0200

    fixed merge conflict

--- db/db.js
+++ db/db.js
@@ -1,11 +1,10 @@
-
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("labs_node_js", "dari", "", {
-    host: "localhost",
-    dialect: "postgres",
-    port: 5432,
-    logging: false,
+const sequelize = new Sequelize("labs_node_js", "postgres", "root", {
+  host: "localhost",
+  dialect: "postgres",
+  port: 5432,
+  logging: false,
 });
 
 module.exports = sequelize;
